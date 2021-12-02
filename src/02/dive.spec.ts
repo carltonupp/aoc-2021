74fd03cdab553e96cd59933d72b409da435d082e
Carlton Upperdine <carlton.upperdine@gmail.com>
2021-12-02 09:44:45 +0000

    initial start

---
+++ src/02/dive.spec.ts
@@ -0,0 +1,9 @@
+import { getPosition } from "./dive";
+
+describe('Dive!', () => {
+    it('Should return correct position given series of commands', () => {
+        const testData = ['forward 5', 'down 5'];
+        const result = getPosition(testData);
+        expect(result).toEqual(25);
+    });
+});
---
+++ src/02/dive.ts
@@ -0,0 +1,70 @@
+type Direction = 'up' | 'down' | 'forward';
+
+class Command {
+    private _direction: Direction;
+    public get direction(): Direction {
+        return this._direction;
+    }
+
+    private _units: number;
+    public get units(): number {
+        return this._units;
+    }
+
+    constructor(direction: string, units: number) {
+        this._units = units;
+
+        if (isDirection(direction)) {
+            this._direction = direction;
+        } else {
+            throw new Error('Invalid direction');
+        }
+    }
+}
+
+function isDirection(direction: string): direction is Direction {
+    return ['up', 'down', 'forward'].includes(direction);
+}
+
+class Position {
+    private vertical: number;
+    private horizontal: number;
+
+    constructor() {
+        this.vertical = 0;
+        this.horizontal = 0;
+    }
+
+    move(cmd: Command) {
+        switch (cmd.direction) {
+            case 'down':
+                this.vertical += cmd.units;
+                break;
+            case 'up':
+                this.vertical -= cmd.units;
+                break;
+            case 'forward':
+                this.horizontal += cmd.units;
+                break;
+            default:
+                throw new Error('Invalid command');
+        }
+    }
+    
+    get() {
+        return this.horizontal * this.vertical;
+    }
+}
+
+
+export function getPosition(commands: string[]): number {
+    let position = new Position();
+
+    for (let command of commands) {
+        const [direction, units] = command.split(' ');
+        const unitsValue = Number.parseInt(units);
+        position.move(new Command(direction, unitsValue));
+    }
+
+    return position.get();
+}
