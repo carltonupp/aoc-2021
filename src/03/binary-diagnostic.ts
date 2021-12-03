facd09a3f1a95e164fc391f19e1b51a3c5bb26a9
Carlton Upperdine <carlton.upperdine@gmail.com>
2021-12-03 17:00:32 +0000

    end of day hacking

--- src/03/binary-diagnostic.ts
+++ src/03/binary-diagnostic.ts
@@ -1,3 +1,29 @@
+import { parse } from 'path/posix';
+
 export function diagnosticReport(numbers: string[]): number {
-    return 0;
+    let gamma = '';
+    let episilon = '';
+
+    for (let i = 0; i < numbers.length; i++) {
+        const digits = numbers.map((n) => n[i]);
+        const mostCommon = mode(digits);
+        const leastCommon = mostCommon === '1' ? '0' : '1';
+        gamma += mostCommon ?? '';
+        episilon += leastCommon;
+    }
+
+    const gammaInt = parseInt(gamma, 2);
+    const episilonInt = parseInt(episilon, 2);
+
+    console.log(gammaInt, episilonInt);
+
+    return parseInt(gamma, 2) * parseInt(episilon, 2);
+}
+
+function mode(array: string[]): string | undefined {
+    return array
+        .sort((a, b) => {
+            return array.filter((v) => v === a).length - array.filter((v) => v === b).length;
+        })
+        .pop();
 }
